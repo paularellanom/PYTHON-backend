19bceec36e5f6bad5700ab9b2965268c9432475e
Paul Arellano <p4u1ro13@gmail.com>
2024-07-11 17:35:27 -0500

    users crud

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -33,6 +33,45 @@ async def user(id: int):
 @app.get("/user/")  
 async def user(id: int):
     return search_user(id)
+
+@app.post("/user/", status_code=201)
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        raise HTTPException(status_code=404, detail="El usuario ya existe")
+    
+    users_list.append(user)
+    return user
+
+@app.put("/user/") 
+async def user(user: User):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+
+    if not found:
+        return {"error": "No se ha actualizado el usuaio"}
+   
+    return user
+
+
+@app.delete("/user/{id}")
+async def user(id: int):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+
+    if not found:
+        return {"error": "No se ha eliminado el usuario"}
+
+
 
 def search_user(id:int):
     users = filter(lambda user: user.id == id, users_list)
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # backend
+<!-- 
+python3 -m venv /home/USER/Documentos/Python/backend/Backend/FastAPI/venv
+
+source /home/USER/Documentos/Python/backend/Backend/FastAPI/venv/bin/activate
+
+which python
+/home/USER/Documentos/Python/backend/Backend/FastAPI/venv/bin/python
+
+pip install fastapi
+
+python -m uvicorn main:app -->
